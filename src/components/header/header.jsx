e42aeefd83d5fdeeaa56f2279566dcc885c0c82f
Carlos(gfcf14) <carloscuba014@gmail.com>
2020-12-28 12:16:50 -0500

    change css files to scss

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { logoImage } from 'images';
-
+import './header.scss';
 
 export function Header() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './app/app';
 import reportWebVitals from './reportWebVitals';
+import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
